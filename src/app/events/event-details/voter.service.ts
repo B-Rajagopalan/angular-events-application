a67f782a32ea31728fd089a038b1020442b30753
B-Rajagopalan <rajagopalan.balamuthukumar@gmail.com>
2024-03-25 09:40:32 +0530

    Disabled http

--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -17,19 +17,19 @@ export class VoterService {
             return voter !== voterName.firstName
         })
             // HTTP Request 
-        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName.firstName}`
-        this.http.delete(url).pipe(catchError(this.handleError('deleteVoter')))
-        .subscribe()
+        // const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName.firstName}`
+        // this.http.delete(url).pipe(catchError(this.handleError('deleteVoter')))
+        // .subscribe()
     }
 
     addVoter(eventId: number, session: ISession, voterName: { firstName: string }) {
         session.voters.push(voterName.firstName)
             // HTTP Request 
-        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName.firstName}`
-        const options = {headers: new HttpHeaders({'Content-Type' : '/application/json'})}
-        this.http.post(url, {}, options)
-         .pipe(catchError(this.handleError('addVoter')))
-         .subscribe()
+        // const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName.firstName}`
+        // const options = {headers: new HttpHeaders({'Content-Type' : '/application/json'})}
+        // this.http.post(url, {}, options)
+        //  .pipe(catchError(this.handleError('addVoter')))
+        //  .subscribe()
     }
 
     userHasVoted(session: ISession, voterName: { firstName: string }): boolean {
